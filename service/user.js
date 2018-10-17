8a1b745bb1d8cabe1abe5dcb4cf733750f93668e
Alpha <just4regist@qq.com>
2018-10-17 12:00:28 +0800

    完善用户模块

--- service/user.js
+++ service/user.js
@@ -1,5 +1,6 @@
 let User = require("../model/user");
 let encryptUtil = require("../utils/encryptUtil");
+let config = require("../config");
 
 /**
  * 用户注册
@@ -9,17 +10,21 @@ let encryptUtil = require("../utils/encryptUtil");
  */
 async function regist(user) {
 
+
     // 根据用户名查询用户
     let result = await findByUsername(user.username);
+
     if (result) {
         throw Error(`用户名${user.username}已经被占用`);
     }
+
     // 把密码进行加密
     // 参数1 : 原文
     // 参数2 : 盐
     user.password = encryptUtil.md5Hmac(user.password, user.username);
     // 对角色重新赋值,避免攻击
     user.role = 0;
+
     // 注册
     result = await User.create(user);
     result.password = "";
@@ -37,7 +42,7 @@ async function deleteUserByUsername(username) {
     // 根据用户名检查用户是否存在
     await isExistByUsername(username);
 
-    result = await User.deleteOne({username: username});
+    let result = await User.deleteOne({username: username});
     if (result.n !== 1) {
         throw Error("删除失败");
     }
@@ -59,7 +64,7 @@ async function isExistByUsername(username) {
  */
 async function findByUsername(username) {
 
-    return await User.findOne({username: username})
+    return await User.findOne({username: username}).select("-password");
 }
 
 /**
@@ -79,10 +84,24 @@ async function login(user) {
 
     // 加密密码
     user.password = encryptUtil.md5Hmac(password, user.username);
-
+    // 查询用户
     user = await User.findOne(user);
-    user.password = "";
-    return user;
+    // 查询用户失败,说明用户名或密码错误
+    if (!user) {
+        throw Error("用户名或密码错误");
+    }
+    // 查询成功,定义token数据
+    let token = {
+        username: user.username,
+        expire: Date.now() + config.TOKEN_EXPIRE
+    };
+
+    // 参数1 : 原文
+    // 参数2 : 密钥
+    // 对token进行加密
+    let encryptedToken = encryptUtil.aesEncrypt(JSON.stringify(token), config.TOKEN_KEY);
+
+    return encryptedToken;
 
 }
 
